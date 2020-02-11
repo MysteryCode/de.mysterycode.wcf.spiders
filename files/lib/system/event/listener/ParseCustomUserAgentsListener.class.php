eeced1eee041fa70227c10d66e873bb32bd8a225
mutec <mysterycode@mysterycode.de>
2020-02-11 13:08:36 +0100

    fix regex for WSC user agent

--- files/lib/system/event/listener/ParseCustomUserAgentsListener.class.php
+++ files/lib/system/event/listener/ParseCustomUserAgentsListener.class.php
@@ -10,7 +10,7 @@ class ParseCustomUserAgentsListener implements IParameterizedEventListener {
 	 */
 	public function execute($eventObj, $className, $eventName, array &$parameters) {
 		/** @var UserOnline $eventObj */
-		if (preg_match('/(WoltLab (Suite|Community Framework))\/([0-9a-zA-Z. ]+)/', $parameters['userAgent'], $matches)) {
+		if (preg_match('/(WoltLab (?:Suite|Community Framework))\/([0-9a-zA-Z. ]+)/', $parameters['userAgent'], $matches)) {
 			$parameters['browser'] = $matches[1] . ' ' . $matches[2];
 		}
 		if (preg_match('/(WSC-Connect (?:API|WSC-Connect Mobile Browser))(?: |\/)([0-9a-zA-Z.\+\- ]+)/', $parameters['userAgent'], $matches)) {
